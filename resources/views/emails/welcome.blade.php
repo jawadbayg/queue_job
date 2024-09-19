a7ca3328b9e847e005ccd93e8d207954d88b30a7
Jawad Bayg <jawadbaygg@gmail.com>
2024-09-19 18:06:30 +0500

    queue job for user registration

---
+++ resources/views/emails/welcome.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome to Our Application</title>
+</head>
+<body>
+    <h1>Hello, {{ $user->name }}!</h1>
+    <p>Welcome to our platform. We're glad to have you here.</p>
+</body>
+</html>
